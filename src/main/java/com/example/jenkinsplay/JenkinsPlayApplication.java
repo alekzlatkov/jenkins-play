d62375fde04b8e2bf1e145ff78a9475a53f2be52
Aleksandar Aleksandrov <alek.zlatkov@gmail.com>
2020-08-03 19:18:39 +0300

    Initial commit

---
+++ src/main/java/com/example/jenkinsplay/JenkinsPlayApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsplay/JenkinsPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
